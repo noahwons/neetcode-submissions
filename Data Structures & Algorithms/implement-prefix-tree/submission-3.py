895402c9c399c91ac1f6d5dce73314b46cc4f7eb
Noah Wons <110370842+noahwons@users.noreply.github.com>
2026-08-05 21:19:49 -0500

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.py
@@ -0,0 +1,47 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                print("adding: ", c)
+                cur.children[c] = TrieNode()
+                cur = cur.children[c]
+            else:
+                cur = cur.children[c]
+        
+        cur.isWord = True
+
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+            else:
+                return False
+        
+        return cur.isWord
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c in cur.children:
+                cur = cur.children[c]
+            else:
+                return False
+        
+        return True
+        
+        
